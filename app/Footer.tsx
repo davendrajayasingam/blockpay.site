c2a22c7653437659c28ed9d68e942ec274c00a87
Davendra Jayasingam <hello@websplorer.com>
2023-08-06 10:52:20 +0800

    Typo fix

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -11,7 +11,7 @@ export default function Footer()
         <footer className='mt-auto'>
             <div className='mt-12 bg-gray-900'>
                 <p className='text-slate-400 text-sm text-center bg-black/20 p-2 border-t border-white/10'>
-                    Made with by ❤️ Davendra Jayasingam
+                    Made with ❤️ by Davendra Jayasingam
                 </p>
             </div>
         </footer>
